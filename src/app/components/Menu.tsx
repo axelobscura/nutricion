16ee2362977bfa0363bbe0ca2791299ad43045d1
Axel <axosar@gmail.com>
2023-02-14 09:57:26 -0600

    menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 
 export default function Menu() {
   return (
-    <div className="wrapHeader">
+    <div className="wrapMenu">
       <Link href='/'>INICIO</Link>
-      <Link href='/registro'>REGISTRO</Link>
       <Link href='/programa'>PROGRAMA</Link>
+      <Link href='/registro'>REGISTRO</Link>
+      <Link href='/login'>INGRESAR</Link>
     </div>
   )
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function Login() {
+  return (
+    <Container className='entrada' fluid>
+      <Row>
+        <Col>
+          <h1>LOGIN</h1>
+        </Col>
+      </Row>
+    </Container>
+  )
+}

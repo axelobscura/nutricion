a707a260e64ed36643ce7cdbd3fdf5f807032bbf
Axel <axosar@gmail.com>
2023-02-14 09:09:45 -0600

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from 'next/image';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -9,6 +9,13 @@ export default function Home() {
     <Container className='entrada' fluid>
       <Row>
         <Col>
+          <Image
+            src='/logo-amgg-blanco.png'
+            width='200'
+            height='150'
+            alt='Curso Latinoamericano en Nutrición y Alimentación Geriátrica'
+            style={{marginBottom: '30px'}}
+          />
           <h1>Curso Latinoamericano en Nutrición y Alimentación Geriátrica</h1>
           <h4>CURSO DE NUTRICIÓN DEL ADULTO MAYOR</h4>
           <h3>16 y 17 de Marzo 2023</h3>

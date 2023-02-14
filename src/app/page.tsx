bf89d9a3c5e3baa831961dd56ae433b8126723db
Axel <axosar@gmail.com>
2023-02-13 19:48:24 -0600

    bkg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-    </main>
+    <Container className='entrada' fluid>
+      <Row>
+        <Col>
+          <h1>Curso Latinoamericano en Nutrición y Alimentación Geriátrica</h1>
+          <h4>CURSO DE NUTRICIÓN DEL ADULTO MAYOR</h4>
+          <h3>16 y 17 de Marzo 2023</h3>
+        </Col>
+      </Row>
+    </Container>
   )
 }

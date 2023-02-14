bf006c0e01413f04d5a8d726bc360e90b5674ced
Axel <axosar@gmail.com>
2023-02-14 09:35:27 -0600

    favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Curso Latinoamericano en Nutrición y Alimentación Geriátrica | Asosiación Mexicana de Geriatría y Gerontología A.C.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Curso Latinoamericano en Nutrición y Alimentación Geriátrica, Asosiación Mexicana de Geriatría y Gerontología A.C." />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </>
   )
 }

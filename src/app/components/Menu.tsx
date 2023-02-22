c8245fc1fc599d7894c535b772cc0aef5d9937cf
axelobscura <axosar@gmail.com>
2023-02-21 20:31:00 -0600

    menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,11 +1,22 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Menu() {
   return (
     <div className="wrapMenu">
-      <Link href='/'>INICIO</Link>
-      <Link href='/registro'>REGISTRO</Link>
-      <Link href='/login'>LOGIN</Link>
+      <div>
+        <Image
+          src='/logo.png'
+          width='350'
+          height='50'
+          alt='Curso Latinoamericano en Nutrición y Alimentación Geriátrica'
+        />
+      </div>
+      <div>
+        <Link href='/'>INICIO</Link>
+        <Link href='/registro'>REGISTRO</Link>
+        <Link href='/login'>LOGIN</Link>
+      </div>
     </div>
   )
 }

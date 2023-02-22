505de73117c5016ae8a05f213c5b1a6897a3f2c5
unknown <axosar@gmail.com>
2023-02-21 22:14:13 -0600

    menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,26 @@ export default function Home() {
           <h3>16 y 17 de Marzo 2023</h3>
         </Col>
       </Row>
+      <Container className='objetivo'>
+        <Row>
+          <Col>
+            <div>
+              <h3>OBJETIVO</h3>
+              <p>Ut fermentum leo sed purus egestas porta. Duis malesuada maximus leo sit amet viverra. Sed nec purus sit amet turpis viverra posuere eget a mi. Nulla nec varius elit. Phasellus vestibulum consequat facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque nec volutpat odio. Ut at varius lacus, lacinia sollicitudin est. Proin mollis quam et libero tincidunt elementum.</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Container className='temario' fluid>
+        <Row>
+          <Col>
+            <div>
+              <h3>Temario General</h3>
+              <p>Ut fermentum leo sed purus egestas porta. Duis malesuada maximus leo sit amet viverra. Sed nec purus sit amet turpis viverra posuere eget a mi. Nulla nec varius elit. Phasellus vestibulum consequat facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque nec volutpat odio. Ut at varius lacus, lacinia sollicitudin est. Proin mollis quam et libero tincidunt elementum.</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   )
 }

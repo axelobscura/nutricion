38874e5f103dbc14807cf4c6141b36b5942171ff
axelobscura <axosar@gmail.com>
2023-02-21 20:18:55 -0600

    menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -4,9 +4,8 @@ export default function Menu() {
   return (
     <div className="wrapMenu">
       <Link href='/'>INICIO</Link>
-      <Link href='/programa'>PROGRAMA</Link>
       <Link href='/registro'>REGISTRO</Link>
-      <Link href='/login'>INGRESAR</Link>
+      <Link href='/login'>LOGIN</Link>
     </div>
   )
 }

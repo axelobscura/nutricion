2725680e476f5880894606ca9a160444f83fe589
axelobscura <axosar@gmail.com>
2023-02-24 17:19:37 -0600

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,37 @@
+"use client";
+import Link from 'next/link'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'next/image'
+
 export default function Footer() {
   return (
-    <div className="wrapFooter">
-      <p>® 2023 Asociación Mexicana de Gerontología y Geriatría A.C.</p>
-    </div>
+    <Container className="wrapFooter" fluid>
+      <Row>
+        <Col>
+          <Link href="/privacidad">Aviso de privacidad</Link>
+        </Col>
+        <Col>
+          <Link href="/privacidad">Términos y condiciones</Link>
+        </Col>
+        <Col>
+          <Link href="/privacidad">
+            <Image
+              src='/logo-amgg-blanco.png'
+              width='70'
+              height='50'
+              alt='Curso Latinoamericano en Nutrición y Alimentación Geriátrica'
+            />
+          </Link>
+        </Col>
+        <Col></Col>
+        <Col>
+          <div>
+            <p>® 2023 Asociación Mexicana de Gerontología y Geriatría A.C.</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }

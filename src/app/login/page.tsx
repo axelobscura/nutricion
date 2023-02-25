dfd329b8ae0e93858b71e796d417b49420658f10
axelobscura <axosar@gmail.com>
2023-02-24 18:30:09 -0600

    footer

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,32 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Banner from '../components/Banner'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 export default function Login() {
   return (
-    <Container className='entrada' fluid>
+    <Container className='registro' fluid>
+      <Banner/>
       <Row>
-        <Col>
-          <h1>LOGIN</h1>
+        <Col></Col>
+        <Col xs={12} md={6} className='reg' style={{ padding: '70px 0'}}>
+          <h3>ACCESO</h3>
+          <hr/>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Correo electrónico:</Form.Label>
+              <Form.Control type="text" name="apaterno" placeholder="Ingrese su correo electrónico" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control type="text" name="amaterno" placeholder="Ingrese su contraseña" />
+            </Form.Group>
+            <Button type="submit" className="btn_login">INGRESAR</Button>
+          </Form>
         </Col>
+        <Col></Col>
       </Row>
     </Container>
   )

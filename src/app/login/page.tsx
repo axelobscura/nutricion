87c9caab25933dcef3d4f260c795aaf387951fe7
axelobscura <axosar@gmail.com>
2023-02-24 18:51:06 -0600

    footer

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,11 +18,11 @@ export default function Login() {
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Correo electrónico:</Form.Label>
-              <Form.Control type="text" name="apaterno" placeholder="Ingrese su correo electrónico" />
+              <Form.Control type="email" name="email" placeholder="Ingrese su correo electrónico" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Contraseña:</Form.Label>
-              <Form.Control type="text" name="amaterno" placeholder="Ingrese su contraseña" />
+              <Form.Control type="password" name="contrasena" placeholder="Ingrese su contraseña" />
             </Form.Group>
             <Button type="submit" className="btn_login">INGRESAR</Button>
           </Form>

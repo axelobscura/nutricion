ee860cbc4549651df7698385a7db1edc6fe82205
axelobscura <axosar@gmail.com>
2023-03-01 05:48:05 -0600

    textos entrada

--- src/app/registro/page.tsx
+++ src/app/registro/page.tsx
@@ -76,6 +76,7 @@ export default function Registro() {
         }),
       })
       setEnviado('true');
+      window.location.href = "https://buy.stripe.com/4gwaEI3GE11UaVGcMM";
       const json = await res.json()
       if (!res.ok) throw Error(json.message)
     } catch(e: any){
